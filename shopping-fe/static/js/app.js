661fd81b745966cf082856826cfacb9d043132ca
Luc Weinbrecht <luc@weinbrecht.de>
2022-11-14 22:48:28 +0100

    added loading animation

--- shopping-fe/static/js/app.js
+++ shopping-fe/static/js/app.js
@@ -1,14 +1,21 @@
 async function submitForm(e, form) {
+    showProgressbar();
     e.preventDefault();
     const jsonFormData = buildJsonFormData(form);
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     const url = `http://localhost:50050/shop/order?${urlSearchParams.toString()}`;
-    const responseStatus = await performPostHttpRequest(url, jsonFormData);
-    if (responseStatus < 200 || responseStatus > 299) {
+    try {
+        const responseStatus = await performPostHttpRequest(url, jsonFormData);
+        if (responseStatus < 200 || responseStatus > 299) {
+            showNotification('An error occurred.', 'is-danger');
+        } else {
+            showNotification('Order is placed', 'is-primary');
+        }
+    } catch(error) {
         showNotification('An error occurred.', 'is-danger');
-    } else {
-        showNotification('Order is placed', 'is-primary');
+    } finally {
+        removeProgressbar();
     }
 }
 
@@ -49,6 +56,16 @@ function showNotification(text, typeClass) {
     element.className = element.className.replace('show', '');
     element.removeChild(msgEl);
     }, 1500);
+}
+
+function showProgressbar() {
+  const progressbar = document.getElementById('progress');
+  progressbar.classList.add('show');
+}
+
+function removeProgressbar() {
+  const progressbar = document.getElementById('progress');
+  progressbar.className = progressbar.className.replace('show', '');
 }
 
 const orderForm = document.querySelector('#orderForm');
